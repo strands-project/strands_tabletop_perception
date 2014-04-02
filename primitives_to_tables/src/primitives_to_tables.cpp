2b2a497942bf5abb236fff54b629e4f142378ae6
Nils Bore <nbore@kth.se>
2014-04-02 16:41:18 +0200

    Added the primitives to tables package that converts the detected primitives to the strands table message and checks if they are already in the datacentre

---
+++ primitives_to_tables/src/primitives_to_tables.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "ros/ros.h"
+
+#include "primitive_extraction/Primitive.h"
+#include "primitive_extraction/PrimitiveArray.h"
+#include "strands_perception_msgs/Table.h"
+
+#include <Eigen/Dense>
+
+ros::Publisher pub;
+
+void callback(const primitive_extraction::PrimitiveArray::ConstPtr& msg)
+{
+    
+    for (int i = 0; i < msg->primitives.size(); ++i) {
+        strands_perception_msgs::Table table;
+        primitive_extraction::Primitive primitive = msg->primitives[i];
+        table.header.frame_id = msg->camera_frame;
+        table.pose.pose = primitive.pose;
+        table.tabletop.points.resize(primitive.points.size());
+        for (int j = 0; j < primitive.points.size(); ++j) {
+            table.tabletop.points[j].x = primitive.points[j].x;
+            table.tabletop.points[j].y = primitive.points[j].y;
+            table.tabletop.points[j].z = primitive.points[j].z;
+        }
+        pub.publish(table);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "primitives_to_tables");
+    ros::NodeHandle n;
+    
+    ros::NodeHandle pn("~");
+	std::string input;
+	pn.param<std::string>("input", input, std::string("/tables"));
+	ros::Subscriber sub = n.subscribe(input, 1, callback);
+	std::string output;
+	pn.param<std::string>("output", output, std::string("/table_tables"));
+	pub = n.advertise<strands_perception_msgs::Table>(output, 1);
+    
+    ros::spin();
+    
+    return 0;
+}
+
