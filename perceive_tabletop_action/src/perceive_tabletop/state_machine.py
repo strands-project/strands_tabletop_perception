d135252b6af22637e0822d3de8852c79cde15623
Chris Burbridge <cburbridge@gmail.com>
2014-04-07 23:25:07 +0100

    fix table transform

--- perceive_tabletop_action/src/perceive_tabletop/state_machine.py
+++ perceive_tabletop_action/src/perceive_tabletop/state_machine.py
@@ -2,6 +2,11 @@
 import rospy
 import smach
 import smach_ros
+import sys
+
+
+from ros_datacentre.message_store import MessageStoreProxy
+from strands_perception_msgs.msg import Table
 
 import actionlib
 from actionlib_msgs.msg import *
@@ -15,11 +20,8 @@ from perceive_tabletop.action_monitor import ActionMonitor
 from perceive_tabletop.view_planning  import ViewPlanning
 from perceive_tabletop.perception     import *
 
-from ros_datacentre.message_store import MessageStoreProxy
-from strands_perception_msgs.msg import Table
-
-import tf
 import numpy
+import tf
 
 MOVE_BASE_EXEC_TIMEOUT=rospy.Duration(600.0)
 MOVE_BASE_PREEMPT_TIMEOUT=rospy.Duration(10.0)
@@ -39,45 +41,48 @@ class PerceiveTabletopSM(smach.StateMachine):
         smach.StateMachine.__init__(self, outcomes=['succeeded',
                                                     'aborted',
                                                     'preempted'],
-                                                    input_keys=['goal'])
+                                                    input_keys=['table_id'])
 
         
 
         #polygon = [[-1.0,0.0],[4.0,0.0],[4.0,-4.0],[-1.0,-4.0]]
 
         self._msg_store=MessageStoreProxy()
-        table=self._get_table("test_table_2") #self.userdata.goal) #
+        table=self._get_table("test_lg_1") #userdata.table_id) #TODO: what if non exist?
         o=table.pose.pose.orientation
-        quat=numpy.array([o.w,o.x,o.y,o.z])
+        quat=numpy.array([o.x,o.y,o.z,o.w])
         mat=tf.transformations.quaternion_matrix(quat)
-        mat[3][0]=table.pose.pose.position.x
-        mat[3][1]=table.pose.pose.position.y
-        mat[3][2]=table.pose.pose.position.z
+        mat[0][3]=table.pose.pose.position.x
+        mat[1][3]=table.pose.pose.position.y
+        mat[2][3]=table.pose.pose.position.z
 
-        #for pt in table.tabletop.points:
-        #    p=numpy.array([pt.x, pt.y, pt.z,1])
-        #    new=numpy.dot(mat,p)[:-1]
-        #    pt.x=new[0]
-        #    pt.y=new[1]
-        #    pt.z=new[2]
+        for pt in table.tabletop.points:
+            p=numpy.array([pt.x, pt.y, pt.z,1])
+            new=numpy.dot(mat,p)[:-1]
+            pt.x=new[0]
+            pt.y=new[1]
+            pt.z=new[2]
 
+        
         # Table in bham lab
-        polygon = [[-1.9,-5.2],
-                   [-1.9,-5.8],
-                   [-3.8,-5.8],
-                   [-3.8,-5.2]]
-         
-        rospy.loginfo('Polygon: %s', polygon)
-        points = []
-        for point in polygon:
-            rospy.loginfo('Point: %s', point)
-            points.append(Point32(float(point[0]),float(point[1]),0))
+#        polygon = [[-1.9,-5.2],
+#                   [-1.9,-5.8],
+#                   [-3.8,-5.8],
+#                   [-3.8,-5.2]]
+#        rospy.loginfo('Polygon: %s', polygon)
+#        points = []
+#        for point in polygon:
+#            rospy.loginfo('Point: %s', point)
+#            points.append(Point32(float(point[0]),float(point[1]),0))
 
-        poly = Polygon(points) #table.tabletop #
+        poly = table.tabletop #Polygon(points) #
+        rospy.loginfo("Table: %s" % poly)
+
         self.userdata.sm_table_area = poly
         self.userdata.sm_table_pose = []
 
         self.userdata.sm_action_completed = False
+
 
         self._action_monitor  = ActionMonitor()
         self._view_planning   = ViewPlanning()
@@ -140,10 +145,8 @@ class PerceiveTabletopSM(smach.StateMachine):
 
     def _get_table(self, table_id):
         """ Get a specific table """
-        # UNUSED.
         query = {}
         query["table_id"] = table_id
         #TODO: what if does not exist....
         return self._msg_store.query(Table._type, query,single=True)[0]
-
-    
+                                                  
