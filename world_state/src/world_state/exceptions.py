f755ad7501ec41d9970b87fe995549d888be0b36
cburbridge <cburbridge@gmail.com>
2014-04-10 13:42:59 +0100

    Adding custom exceptions

---
+++ world_state/src/world_state/exceptions.py
@@ -0,0 +1,18 @@
+class ObjectMasterException(Exception):
+    """Exceptions realted to the object master. """
+    def __init__(self, tp, additional_info=None):
+        self.types={}
+        self.types["UNKNOWN_CAT"]="Object class not known."
+        self.types["UNKNOWN_INSTANCE"]="Object instance not known."
+
+        self.tp = tp
+        self.additional_info=additional_info
+        
+    def __str__(self):
+        if self.additional_info:
+            return '\n\n' + str(self.types[self.tp] + '\n'+str(self.additional_info))
+        else:
+            return '\n\n' +repr(self.types[self.tp] )
+        
+    
+    
