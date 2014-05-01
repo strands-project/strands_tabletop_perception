e7c189213f6af964c06cb3a0f5f9360139133667
cburbridge <cburbridge@gmail.com>
2014-05-01 18:21:21 +0100

    indentation tidy up

--- perceive_tabletop_action/src/perceive_tabletop/state_machine.py
+++ perceive_tabletop_action/src/perceive_tabletop/state_machine.py
@@ -26,14 +26,14 @@ MOVE_BASE_PREEMPT_TIMEOUT=rospy.Duration(10.0)
 
 #callback that build the move_base goal, from the input data        
 def move_base_goal_cb(userdata,goal):
-    
+
     next_goal = strands_navigation_msgs.msg.MonitoredNavigationGoal() # move_base_msgs.msg.MoveBaseGoal()            
     next_goal.target_pose.header.frame_id = "/map"
     next_goal.target_pose.header.stamp = rospy.Time.now()
     next_goal.target_pose.pose = userdata.pose_input
 
     next_goal.action_server = "move_base"
-    
+
     return next_goal
 
 class PerceiveTabletopSM(smach.StateMachine):
@@ -56,49 +56,48 @@ class PerceiveTabletopSM(smach.StateMachine):
         else: # 'sim'
             self._perception = PerceptionSim()
 
-        
-        with self:
-             smach.StateMachine.add('ActionMonitor', self._action_monitor, 
-                                    transitions={'succeeded': 'succeeded',
-                                                 'action_in_progress':'ViewPlanning',
-                                                 'aborted':'aborted',
-                                                 'preempted':'preempted'},
-                                    remapping={'obj_list':'sm_obj_list' #  ,
-                                               }) #'action_completed':'sm_action_completed'
 
-             smach.StateMachine.add('ViewPlanning', self._view_planning, 
-                                    transitions={'succeeded':'Navigation',
-                                                 'action_completed':'ActionMonitor',
-                                                 'aborted':'aborted',
-                                                 'preempted':'preempted'},
-                                    remapping={'obj_list':'sm_obj_list',
-                                               'table_pose':'sm_table_pose',
-                                               'pose_output':'sm_pose_data',
-                                               'view_list':'sm_view_list' #,
-                                               }) #                                               'action_completed':'sm_action_completed'
-             
-             # The navigation is realized via Move Base directly
-             # TODO: replace with monitored navigation in strands_navigation
-             smach.StateMachine.add('Navigation',
-                                    smach_ros.SimpleActionState('monitored_navigation',
-                                                                MonitoredNavigationAction,
-                                                                goal_cb = move_base_goal_cb,
-                                                                #result_cb = move_base_result_cb,
-                                                                input_keys = ['pose_input'],
-                                                                exec_timeout = MOVE_BASE_EXEC_TIMEOUT,
-                                                                preempt_timeout = MOVE_BASE_PREEMPT_TIMEOUT
-                                                                ),
-                                    transitions={'succeeded':'Perception',
-                                                 'aborted':'aborted',
-                                                 'preempted':'preempted'},
+        with self:
+            smach.StateMachine.add('ActionMonitor', self._action_monitor, 
+                                   transitions={'succeeded': 'succeeded',
+                                                'action_in_progress':'ViewPlanning',
+                                                'aborted':'aborted',
+                                                'preempted':'preempted'},
+                                   remapping={'obj_list':'sm_obj_list' #  ,
+                                              }) #'action_completed':'sm_action_completed'
+
+            smach.StateMachine.add('ViewPlanning', self._view_planning, 
+                                   transitions={'succeeded':'Navigation',
+                                                'action_completed':'ActionMonitor',
+                                                'aborted':'aborted',
+                                                'preempted':'preempted'},
+                                   remapping={'obj_list':'sm_obj_list',
+                                              'table_pose':'sm_table_pose',
+                                              'pose_output':'sm_pose_data',
+                                              'view_list':'sm_view_list' #,
+                                              }) #                                               'action_completed':'sm_action_completed'
+
+            # The navigation is realized via Move Base directly
+            # TODO: replace with monitored navigation in strands_navigation
+            smach.StateMachine.add('Navigation',
+                                   smach_ros.SimpleActionState('monitored_navigation',
+                                                               MonitoredNavigationAction,
+                                                               goal_cb = move_base_goal_cb,
+                                                               #result_cb = move_base_result_cb,
+                                                               input_keys = ['pose_input'],
+                                                               exec_timeout = MOVE_BASE_EXEC_TIMEOUT,
+                                                               preempt_timeout = MOVE_BASE_PREEMPT_TIMEOUT
+                                                               ),
+                                   transitions={'succeeded':'Perception',
+                                                'aborted':'aborted',
+                                                'preempted':'preempted'},
                                    remapping={'pose_input':'sm_pose_data'},
                                    )
-             smach.StateMachine.add('Perception', self._perception, 
-                                    transitions={'succeeded':'ActionMonitor',
-                                                 'aborted':'aborted',
-                                                 'preempted':'preempted'},
-                                    remapping={'view_list':'sm_view_list',
-                                               'obj_list':'sm_obj_list'}
-                                    )
-
+            smach.StateMachine.add('Perception', self._perception, 
+                                   transitions={'succeeded':'ActionMonitor',
+                                                'aborted':'aborted',
+                                                'preempted':'preempted'},
+                                   remapping={'view_list':'sm_view_list',
+                                              'obj_list':'sm_obj_list'}
+                                   )
 
