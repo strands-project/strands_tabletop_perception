75391fc80eb4b7bf3b529804dbebcc92dfdfe077
cburbridge <cburbridge@gmail.com>
2014-05-02 15:33:49 +0100

    ..

--- world_state/src/world_state/observation.py
+++ world_state/src/world_state/observation.py
@@ -5,7 +5,8 @@ class Observation(object):
     pass
 
 class MessageStoreObject(mongo.MongoTransformable):
-    def __init__(self,  database, collection, obj_id):
+    def __init__(self,  database, collection, obj_id, typ):
         self.database = database
         self.collection = collection
         self.obj_id = obj_id
+        self.typ = typ
