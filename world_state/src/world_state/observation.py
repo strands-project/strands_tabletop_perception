12700ab20e1f794230d481a46f0b281ce288e1a7
cburbridge <cburbridge@gmail.com>
2014-05-02 15:00:31 +0100

    messagestore object wrapper

--- world_state/src/world_state/observation.py
+++ world_state/src/world_state/observation.py
@@ -1,4 +1,11 @@
+import mongo
 
 class Observation(object):
     # Snap shot of robot sensors & actuators
     pass
+
+class MessageStoreObject(mongo.MongoTransformable):
+    def __init__(self,  database, collection, obj_id):
+        self.database = database
+        self.collection = collection
+        self.obj_id = obj_id
