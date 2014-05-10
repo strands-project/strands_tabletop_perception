96f9d9c89475045aa73ea770f5367bad995a920d
cburbridge <cburbridge@gmail.com>
2014-05-10 12:59:15 +0100

    Point cloud visualiser

--- world_state/src/world_state/report.py
+++ world_state/src/world_state/report.py
@@ -1,3 +1,8 @@
+import rospy
+from sensor_msgs.msg import PointCloud2, PointField
+import sensor_msgs.point_cloud2 as pc2
+
+
 from state import World, Object
 def generate_report(t, parent_object=None):
     """
@@ -17,3 +22,37 @@ def generate_report(t, parent_object=None):
             print "-> -> ", o2.name, " ==> ", o2.identification.class_type
     pass
 
+
+class PointcloudVisualiser(object):
+    def __init__(self, topic="pointcloud_visualise"):
+        self._pointclouds = []
+        self._pub = rospy.Publisher(topic, PointCloud2)
+    
+    def add_cloud(self, cld):
+        assert isinstance(cld, PointCloud2)
+        if len(self._pointclouds) > 0:
+            assert cld.point_step == self._pointclouds[0].point_step
+            # TODO: dirty assumption of the fields being the same
+            # TODO: dirty assumption of header being the same
+        self._pointclouds.append(cld)
+    
+    def clear(self):
+        self._pointclouds = []
+        
+    def publish(self):
+        if len(self._pointclouds) < 1:
+            return
+        pts = []
+        colours = [0xFF0000, 0x00FF00, 0x0000FF, 
+                   0xFFFF00, 0xFF00FF, 0x00FFFF]
+        for i, cloud in enumerate(self._pointclouds):
+            rgb =  colours[i%len(colours)]
+            pts.extend([ (p[0], p[1], p[2], rgb) for p in pc2.read_points(cloud,
+                                                                 ['x', 'y', 'z']) ])
+        fields = [PointField('x', 0, PointField.FLOAT32, 1),
+                  PointField('y', 4, PointField.FLOAT32, 1),
+                  PointField('z', 8, PointField.FLOAT32, 1), 
+                  PointField('rgb', 12, PointField.UINT32, 1)]
+        pointcloud = pc2.create_cloud(self._pointclouds[0].header,
+                                      fields, pts)
+        self._pub.publish(pointcloud)
