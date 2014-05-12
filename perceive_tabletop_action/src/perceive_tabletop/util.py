7062429dcae55334664447ae345716fb3ab5504f
cburbridge <cburbridge@gmail.com>
2014-05-12 19:42:13 +0100

    utils to help register tasks; get table and topological node

---
+++ perceive_tabletop_action/src/perceive_tabletop/util.py
@@ -0,0 +1,23 @@
+from world_state.state import World
+
+def get_table(table_name):
+    world = World()
+    if not world.does_object_exist(table_name):
+        rospy.logerr("Object (%s) does not exist"%table_name)
+        return None
+    
+    table =  world.get_object(table_name)
+    if table.identification.class_type[0] != "Table":
+        rospy.logerr("Object (%s) is not a table!"%userdata.table_id)
+        return None
+
+    return table
+    
+def get_node_for_table(table_name):
+    """
+    Given a table name, returns a topological map node name
+    """
+    table = get_table(table_name)
+    if table is None:
+        return 
+    return table.topological_node
